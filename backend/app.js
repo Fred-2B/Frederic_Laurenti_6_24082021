984e1fa5a05cb9e70c199dfccca1783ea8ba3bbe
Fred-2B <flaurenti@free.fr>
2021-08-31 15:13:13 +0200

    Ajout projet

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
+const morgan = require('morgan');
+const helmet = require('helmet');
+require('dotenv').config();
+const session = require('express-session');
+
+
+/* Connexion à la base de données */
+mongoose.connect('mongodb+srv://fred-2b:8578Mongo@cluster0.sjbzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+/* Lancement Express */
+const app = express();
+
+/* Configuration CORS */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.AUTHORIZED_ORIGIN);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,9 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
+/*  Connexion exports.signup = (req, res, next) */
+exports.signup = (req, res, next) => {
+
+
+};
