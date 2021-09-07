885df32c191ec3d9684e75f610842c349b9fb24d
Fred-2B <flaurenti@free.fr>
2021-09-07 11:36:27 +0200

    Ajout cookie-session

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,10 @@ const userRoutes = require('./routes/user');
 // Donne accès au chemin de notre système de fichier "images"
 const path = require('path');
 
+// Utilisation du module HELMET pour la sécurité, protége l'application de certaines vulnérabilités avec en complement l'implementation du middleware COOKIE-SESSION
+const helmet = require('helmet');
+const session = require('cookie-session');
+
 // utilisation DOTENV pour masquer les informations de connexion à la base de données à l'aide de variables d'environnement
 require('dotenv').config();
 
@@ -32,14 +36,29 @@ const app = express();
 
 // Middleware HEADER evite les erreurs de securité CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Permet d'accéder à notre API depuis n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Ajoute les headers mentionnés aux requêtes envoyées vers notre API 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Permet les méthodes autorisées pour les requêtes HTTP
+    // Permet d'accéder à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Ajoute les headers mentionnés aux requêtes envoyées vers notre API 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Permet les méthodes autorisées pour les requêtes HTTP  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
     next();
 });
 
+// Securite cookies
+const expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 heure
+app.use(session({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  cookie: { secure: true,
+            httpOnly: true,
+            domain: 'http://localhost:3000',
+            expires: expiryDate
+  }
+  })
+);
 
-//Transforme les données POST en objet JSON
+//Transforme la requete POST en objet JSON
 app.use(bodyParser.json());
 
 // Midleware qui permet de charger les fichiers qui sont dans le repertoire images
@@ -51,6 +70,8 @@ app.use('/api/sauces', saucesRoutes);
 // Va servir les routes dédiées pour l'authentification des utilisateurs
 app.use('/api/auth', userRoutes);
 
+// Securise EXPRESS
+app.use(helmet());
 
 // Exporte EXPRESS dans server.js
 module.exports = app;
